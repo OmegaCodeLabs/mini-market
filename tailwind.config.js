406a98408ca9cb9006ebc1a50af06eb73691b5ba
Salvador <68579066+Chava888@users.noreply.github.com>
2025-12-26 10:08:54 -0800

    Add config for tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+  ],
+}
